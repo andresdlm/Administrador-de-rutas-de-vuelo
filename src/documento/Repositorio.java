930d30128fca4be42855af37d5b5cde164b7e3dc
Andres Linarez <andreslinarez9@gmail.com>
2019-02-08 15:46:06 -0400

    Se agregaron metodos a la clase repositorio

--- src/documento/Repositorio.java
+++ src/documento/Repositorio.java
@@ -4,12 +4,25 @@ import java.io.*;
 
 public class Repositorio {
 
-    private static File repo_ciudad = new File("C:\\Users\\andre\\Documents\\Estructura de Datos\\Proyecto 1\\Tatteo\\src\\documento\\repositorio_ciudades.txt");
-    private static File repo_vuelos = new File("C:\\Users\\andre\\Documents\\Estructura de Datos\\Proyecto 1\\Tatteo\\src\\documento\\repositorio_vuelos.txt");
-    private static String[] ciudad = new String[5];
+    private static String UbicacionRepoCiudad = "C:\\Users\\andre\\Documents\\Estructura de Datos\\Proyecto 1\\Tatteo\\src\\documento\\repositorio_ciudades.txt";
+    private static String UbicacionRepoVuelos = "C:\\Users\\andre\\Documents\\Estructura de Datos\\Proyecto 1\\Tatteo\\src\\documento\\repositorio_vuelos.txt";
+    private static File repo_ciudad = new File(UbicacionRepoCiudad);
+    private static File repo_vuelos = new File(UbicacionRepoVuelos);
+    private static String[] linea = new String[5];
+
+    private static String[] ciudad = new String[linea.length];
+    private static String[] aeropuerto = new String[linea.length];
+    private static String[] pais = new String[linea.length];
+
+    public static void setUbicacionArchivo(String ubicacionArchivo) {
+        UbicacionRepoCiudad = ubicacionArchivo;
+    }
+
+    public static void setUbicacionRepoVuelos(String ubicacionRepoVuelos) {
+        UbicacionRepoVuelos = ubicacionRepoVuelos;
+    }
 
     public void IngresarCiudad(String Ciudad, String Aeropuerto, String Pais) {
-
         try {
 
             FileWriter fileWriter = new FileWriter(repo_ciudad, true);
@@ -27,12 +40,21 @@ public class Repositorio {
         BufferedReader bufferedReader = new BufferedReader(fileReader);
         String lines;
 
-        for(int j=0;j<ciudad.length;j++){
+        for(int j=0;j<linea.length;j++){
             if(( lines = bufferedReader.readLine()) != null){
-                ciudad[j] = lines;
+                linea[j] = lines;
             }
         }
+    }
 
+    public void SepararString(){
+        for (int i = 0; i < linea.length; i++) {
+            if(linea[i] != null){
+                ciudad[i] = linea[i].split(",")[0];
+                aeropuerto[i] = linea[i].split(",")[1];
+                pais[i] = linea[i].split(",")[2];
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -41,12 +63,14 @@ public class Repositorio {
 
         try {
             repo.leer_ciudad();
+            repo.SepararString();
         }catch (IOException e){
             System.out.println("No se encontro el archivo");
         }
-        for (int i = 0; i < ciudad.length ; i++) {
-            System.out.println(ciudad[i] + "");
+        for (int i = 0; i < linea.length ; i++) {
+            if(linea[i] != null){
+                System.out.println(ciudad[i] + "\n");
+            }
         }
-        System.out.println("Hola");
     }
 }
